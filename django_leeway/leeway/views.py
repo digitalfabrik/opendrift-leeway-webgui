7c7b574ce7718c43569d9066c2b31ed342dc317b
Sven Seeberg <mail@sven-seeberg.de>
2022-12-05 18:43:27 +0100

    Improve GUI messages

--- django_leeway/leeway/views.py
+++ django_leeway/leeway/views.py
@@ -14,7 +14,11 @@ def simulation_form(request):
         lwform.instance.user = request.user
         if lwform.is_valid():
             lwform.save()
-            messages.add_message(request, messages.INFO, 'Request saved. You will receive an e-mail to {} when the simulation is finished.'.format(request.user.email))
+            messages.add_message(request, messages.INFO,
+                'Request saved. You will receive an e-mail to {} when the simulation is finished. Your request ID is {}.'.format(
+                    request.user.email, lwform.instance.uuid
+                )
+            )
             run_leeway_simulation.apply_async([lwform.instance.uuid])
 
     context = {'form': LeewaySimulationForm()}
