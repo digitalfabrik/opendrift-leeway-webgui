3c8715eb720bbc2eb9898bb2efa826ac94ca614b
Sven Seeberg <mail@sven-seeberg.de>
2022-12-07 11:08:05 +0100

    Allow DMS input, fix #8

--- django_leeway/leeway/forms.py
+++ django_leeway/leeway/forms.py
@@ -1,7 +1,46 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 from leeway.models import LeewaySimulation
+from dms2dec.dms_convert import dms2dec
 
 class LeewaySimulationForm(ModelForm):
-    class Meta:
+    """
+    Add a form for simulations with some tweaks of the default.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Use CharField for latitude and longitude to allow DMS input
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['longitude'] = CharField(
+            help_text='Input in decimal and degrees minutes seconds supported.')
+        self.fields['latitude'] = CharField(
+            help_text='Input in decimal and degrees minutes seconds supported.')
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """
+        Configure form fields and help text.
+        """
         model = LeewaySimulation
         fields = ['longitude', 'latitude', 'object_type', 'start_time', 'duration']
+
+        help_texts = {
+            'duration': 'Length of simulation in hours.'
+        }
+
+    def clean_longitude(self):
+        """
+        Convert longitude DMS to decimal
+        """
+        data = self.cleaned_data.get('longitude', '')
+        if "°" in data:
+            return dms2dec(data)
+        return data
+
+    def clean_latitude(self):
+        """
+        Convert latitude DMS to decimal
+        """
+        data = self.cleaned_data.get('latitude', '')
+        if "°" in data:
+            return dms2dec(data)
+        return data
