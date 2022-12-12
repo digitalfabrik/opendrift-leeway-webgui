594e572e6b9fe9d0b0c0a0309a866c9a58e7ebfd
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-12-12 02:40:23 +0100

    Serve simulations in debug mode
    - Add new setting `SIMULATION_URL` which contains the URL where the simulations are served
    - Only serve output directory
    - Adapt template and example apache config

--- opendrift_leeway_webgui/leeway/urls.py
+++ opendrift_leeway_webgui/leeway/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 from . import views
@@ -7,3 +9,17 @@ urlpatterns = [
     path('', views.simulation_form, name='home'), # new
     path('simulations/list/', views.simulations_list, name='simulations_list')
 ]
+
+# Serve simulation files in debug mode
+if settings.DEBUG:
+    urlpatterns += [
+        path(
+            "",
+            include(
+                (
+                    static(settings.SIMULATION_URL, document_root=settings.SIMULATION_OUTPUT),
+                    "simulation_files",
+                )
+            ),
+        ),
+    ]
