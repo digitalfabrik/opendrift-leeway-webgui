e92265b2567db79b9c9048ac1a797993620a5525
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-12-12 02:50:34 +0100

    Add isort GitHub Action

--- opendrift_leeway_webgui/leeway/views.py
+++ opendrift_leeway_webgui/leeway/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 from .forms import LeewaySimulationForm
 from .models import LeewaySimulation
